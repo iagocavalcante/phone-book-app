d440598463092fe1d24170017d9a3a62474c0d90
Iago Cavalcante <iagoangelimc@gmail.com>
2022-10-13 11:27:19 -0300

    ci: remove lint step for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -57,7 +57,7 @@ function App() {
     (async () => {
       await getContacts();
     })()
-  }, []);
+  }, [getContacts]);
 
   const closeModalAndRefreshContacts = async () => {
     setShowModal(false);
